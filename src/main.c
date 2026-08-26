1bf1882fbd588217c983463e7840448420096b76
otavio tavares <otavio.sabino@ufv.br>
2026-08-26 12:26:38 -0300

    iniciando matriz

--- src/main.c
+++ src/main.c
@@ -1,79 +1,91 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void exibeBorda(){
+void criaMatrizVazia(int caracteres[20][80]){
     for(int i = 0; i < 80; i++){
-        printf("-");
+        caracteres[0][i] = '-';
+        caracteres[19][i] = '-';
+
+        if(i < 19 && i > 0){
+            caracteres[i][0] = '|';
+            caracteres[i][79] = '|';
+        }
+
     }
-    printf("\n");
+
+    for(int i = 1; i < 19; i++){
+        for(int j = 1; j < 79; j++){
+
+            caracteres[i][j] = ' ';
+            
+        }
+    }
+
 }
 
-void asteriscoSimples(){
-    exibeBorda();
+void imprimeMatriz(int caracteres[20][80]){
+    for(int i = 0; i < 20; i++){
+        for(int j = 0; j < 80; j++){
 
-
-    exibeBorda();
+            printf("%c", caracteres[i][j]);
+            
+        }
+        printf("\n");
+    }
 }
 
-void soma(){
-    exibeBorda();
+void asteriscoSimples(int caracteres[20][80], int n){
 
 
-    exibeBorda();
-}
-
-void x(){
-    exibeBorda();
 
 
-    exibeBorda();
-}
-
-void aleatorio(){
-    exibeBorda();
-
-
-    exibeBorda();
-}
-
-void especial(){
-    exibeBorda();
-
+    imprimeMatriz(caracteres);
     
-    exibeBorda();
 }
+
+
 
 
 int main(){
 
+    char caracteres[20][80];
+
     
+
+
+
     printf("PROGRAMA GERADOR DE OBRA DE ARTE:\n");
     printf("=================================\n");
     printf("Escolha o tipo de figura:\n1 - Asterisco simpes\n2 - Simbolo de soma com asteriscos\n");
-    printf("3 - Letra x com asteriscos\n4 - Figuras aleatorias\n5 - Obra de arte especial");
-    
+    printf("3 - Letra x com asteriscos\n4 - Figuras aleatorias\n5 - Obra de arte especial\n");
+
     int op;
     scanf(&op);
+    //getchar();
+
+    printf("%d", op);
+
 
     switch(op){
         case 1:
-            asteriscoSimples();
+            //verificar numero
+            asteriscoSimples(caracteres);
             break;
 
         case 2:
-            soma();
+            //soma();
             break;
 
         case 3:
-            x();
+            //x();
             break;
 
         case 4:
-            aleatorio();
+            //aleatorio();
             break;
 
         case 5:
-            especial();
+            //especial();
             break;
 
         default:
